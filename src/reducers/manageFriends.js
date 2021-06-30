3efc9172e9922f5014b934cfbb2ea0d850d8a03a
gajen <meetgajen@yahoo.com>
2021-06-30 16:58:17 -0400

    commit tree a9c5957c97f0577f0d6014cfe5f4fbc14378b9af

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,24 @@
 export function manageFriends(state, action){
-}
+    switch (action.type) {
+      case "ADD_FRIEND":
+       
+        return {
+          ...state,
+          friends: [...state.friends, action.payload]
+        }
+      case "REMOVE_FRIEND":
+        let indexToRemove = state.friends.findIndex( friend =>
+          friend.id === action.payload );
+ 
+        let newFriends = [
+          ...state.friends.slice(0, indexToRemove),
+          ...state.friends.slice(indexToRemove + 1)
+        ]
+  
+       
+        return {...state, friends: newFriends};
+      default:
+        return state;
+  
+    }
+  }
